37bfb02d0667c4a3d32e6f9880a36f3a815ccb19
Azhar Khan <37261552+AzharKhanOfficial@users.noreply.github.com>
2024-07-18 23:30:37 +0530

    Update settings.py  as DB not configured

--- Dieter/settings.py
+++ Dieter/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'Dieter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'dieter',
-        'USER': 'postgres',
-        'PASSWORD': 'meriaysha',
-        'HOST':'localhost'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
